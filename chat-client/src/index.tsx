199250ebb887ea64d86deb2dc055826975730fd7
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-12-14 13:06:58 +0900

    add

--- chat-client/src/index.tsx
+++ chat-client/src/index.tsx
@@ -1,8 +1,23 @@
 import React from "react"
 import { render } from "react-dom"
+import { ThemeProvider, CSSReset, Box } from "@chakra-ui/core"
+import styled from "@emotion/styled"
+// import { layout, system } from "styled-system"
+
+const Layout = styled(Box)`
+  width: 500px;
+  margin: 0 auto;
+`
 
 const App = () => {
-  return <div>hello</div>
+  return (
+    <ThemeProvider>
+      <CSSReset />
+      <Layout>
+        <div>hello</div>
+      </Layout>
+    </ThemeProvider>
+  )
 }
 
 render(<App />, document.querySelector("#root"))
