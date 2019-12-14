ab0e8fc6b346ca9be17d2e65138067aa300de85f
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-12-14 10:54:02 +0900

    wip

--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -1,1 +1,11 @@
-CREATE TABLE dogs (id )
+
+CREATE TABLE public.bleeds (
+    id SERIAL NOT NULL,
+    name text NOT NULL
+);
+
+CREATE TABLE public.dogs (
+    id SERIAL NOT NULL,
+    name text NOT NULL,
+    bleed_id integer
+);
